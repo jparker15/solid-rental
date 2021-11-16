e8f591b7ed7f1784be9cf72617835760343ccf31
Jeremy <jeremyeparker@yahoo.com>
2021-11-16 18:15:29 -0500

    first entity model

--- src/main/java/com/atsignjar/expressian/models/Vehicle.java
+++ src/main/java/com/atsignjar/expressian/models/Vehicle.java
@@ -1,2 +1,66 @@
-package com.atsignjar.expressian.models;public class Vehicle {
+package com.atsignjar.expressian.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicle {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String make;
+    private String model;
+    private int year;
+    private boolean isNew;
+
+    public Vehicle(){}
+
+    public Vehicle(Long id, String make, String model, int year, boolean isNew) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isNew = isNew;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
 }
