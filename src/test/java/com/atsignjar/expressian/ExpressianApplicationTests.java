03773a9690cae67a35d689bb3176ebc3a310c8b3
Jeremy <jeremyeparker@yahoo.com>
2021-11-16 18:16:09 -0500

    first commit

---
+++ src/test/java/com/atsignjar/expressian/ExpressianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsignjar.expressian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
