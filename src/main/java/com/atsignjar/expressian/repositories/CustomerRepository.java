762011404d25adf9978cdbdcfa039855a11067de
Jeremy <jeremyeparker@yahoo.com>
2021-11-16 19:16:07 -0500

    new repo

--- src/main/java/com/atsignjar/expressian/repositories/CustomerRepository.java
+++ src/main/java/com/atsignjar/expressian/repositories/CustomerRepository.java
@@ -1,2 +1,7 @@
-package com.atsignjar.expressian.repositories;public interface CustomerRepository {
+package com.atsignjar.expressian.repositories;
+
+import com.atsignjar.expressian.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
 }
