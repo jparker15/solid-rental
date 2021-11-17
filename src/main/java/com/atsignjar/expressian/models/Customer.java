d4bd6cb5a183151b0dc5a74a033947e68cfc9fa7
Jeremy <jeremyeparker@yahoo.com>
2021-11-16 19:15:57 -0500

    new entity model

--- src/main/java/com/atsignjar/expressian/models/Customer.java
+++ src/main/java/com/atsignjar/expressian/models/Customer.java
@@ -1,2 +1,47 @@
-package com.atsignjar.expressian.models;public class Customer {
+package com.atsignjar.expressian.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    private @Id
+    @GeneratedValue()
+    Long id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
